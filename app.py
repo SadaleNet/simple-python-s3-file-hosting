ad1ee9f75a2b85219076ae10566983729e359603
SadaleNet <SadaleNet@users.noreply.github.com>
2018-11-13 17:06:14 +0800

    No longer using signed URL for file access. Using CloudFlare to cache the files instead

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from jinja2 import Template
 from crossdomain import crossdomain
 app = Flask(__name__)
 
-S3_UPLOAD_GRACE_PERIOD = 30
-S3_ACCESS_GRACE_PERIOD = 30
+S3_UPLOAD_GRACE_PERIOD = 10
 
 def get_s3_current_servicec_provider_envvar(var):
     return os.getenv(f"S3_{os.getenv('S3_CURRENT_SERVICE_PROVIDER')}_{var}")
@@ -51,12 +50,4 @@ def uploaded():
 def result(service_provider, filename):
     with open('result.html') as f:
         template = Template(f.read())
-    s3 = boto3.client('s3')
-    presignedUrl = s3.generate_presigned_url('get_object',
-        Params = {
-            'Bucket': get_s3_current_servicec_provider_envvar("BUCKET"),
-            'Key': filename
-        },
-        ExpiresIn=S3_ACCESS_GRACE_PERIOD
-    )
-    return template.render(fileName=presignedUrl)
+    return template.render(fileName=f"{get_s3_current_servicec_provider_envvar('CLOUDFLARE_ROOT')}/{filename}")
